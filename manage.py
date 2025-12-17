55d69662d23ebafcd6ef4ca365a7cb806910bf65
Ali-ahmad12-ux <dk12to34ra56@gmail.com>
2025-12-17 21:59:56 +0300

    Fix project structure and dependencies

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 ﻿#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+# أضف هذا السطر
+sys.path.append(os.path.join(os.path.dirname(__file__), 'blogicum'))
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogicum.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,7 +14,6 @@ def main():
             "Couldn't import Django. Are you sure it's installed?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
